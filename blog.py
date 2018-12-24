b5d0947e37444f69fb688b4ee0f61fe1fffa4c20
Adeel Ahmed Khan <adeelbarki@gmail.com>
2018-12-24 20:42:26 +0500

    Add references and author

--- blog.py
+++ blog.py
@@ -3,13 +3,13 @@ app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Adeel Barki',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Corey Schafer',
+        'author': 'Adeel Barki',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
