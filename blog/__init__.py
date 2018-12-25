4426b46ee1e94f53eae97e92005091cee0d25fd3
Adeel Ahmed Khan <adeelbarki@gmail.com>
2018-12-25 22:55:46 +0500

    Add logout functionality

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,7 @@
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,6 +9,7 @@ app.config['SECRET_KEY'] = '2d28d1ae605d4a9164cb4a85d75a0477'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 from blog import routes
